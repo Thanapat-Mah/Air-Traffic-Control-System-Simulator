f8bcb7d938e19510662f512c1b4ee362e0613edc
Thanapat-Mah <thanapat.mah@mail.kmutt.ac.th>
2021-10-06 13:40:43 +0700

    Edit visibility of Screen's attributes

--- screen.py
+++ screen.py
@@ -3,22 +3,26 @@ from color import Color
 
 ### screen of program, adjust and display component.
 class Screen:
-	def __init__(self, fullscreen=False, width=1500, height=750, background_color=Color.dark_gray):
-		self.fullscreen = fullscreen
+	def __init__(self, fullscreen=False, width=1500, height=750, background_color=Color.white):
+		self.__fullscreen = fullscreen
 		# if fullsreen, adjust width and height to fit user's display size
 		if fullscreen:
 			info_object = pygame.display.Info()
-			self.width = info_object.current_w
-			self.height = info_object.current_h
+			self.__width = info_object.current_w
+			self.__height = info_object.current_h
 		else:
-			self.width = width
-			self.height = height
-		self.background_color = background_color
-		self.display = pygame.display.set_mode((self.width, self.height))
+			self.__width = width
+			self.__height = height
+		self.__background_color = background_color
+		self.display = pygame.display.set_mode((self.__width, self.__height))
+
+	# return the size of screen in format (width, height)
+	def get_size(self):
+		return((self.__width, self.__height))
 
 	# refresh plain color background
 	def refresh_background(self):
-		self.display.fill(self.background_color)
+		self.display.fill(self.__background_color)
 
 	# update screen by re-draw every components
 	def update_screen(self):
