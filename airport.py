20169f8e99dd303e39ef499abdbef8aa50f22d05
Thanapat-Mah <thanapat.mah@mail.kmutt.ac.th>
2021-10-13 16:55:11 +0700

    Add color and font as an attribute of AirportManager

--- airport.py
+++ airport.py
@@ -12,7 +12,7 @@ class Airport :
 
 
 class AirportManager :
-    def __init__(self, screen_size):
+    def __init__(self, screen_size, airport_color=Color.black, text_color=Color.black, font=Font.bebasneue_normal):
         airport_list = [
             Airport("CNX", 800, 200),
             Airport("BKK", 905, 488),
@@ -25,6 +25,9 @@ class AirportManager :
             airport.x = (airport.x/1920)*screen_size[0]
             airport.y = (airport.y/1080)*screen_size[1]
         self.__airport_tuple = tuple(airport_list)
+        self.__airport_color = airport_color
+        self.__text_color = text_color
+        self.__font = font
 
     def draw_airport(self, display, map_, simulator):
         top_left_point = map_.get_top_left_point()
@@ -37,7 +40,7 @@ class AirportManager :
         for airport in self.__airport_tuple:
             airport_x = (airport.x*scale)+top_left_point[0]
             airport_y = (airport.y*scale)+top_left_point[1]
-            pygame.draw.circle(display, Color.black, (airport_x, airport_y), self.__airport_size)
+            pygame.draw.circle(display, self.__airport_color, (airport_x, airport_y), self.__airport_size)
 
-            text = Font.bebasneue_normal.render(airport.name, True, Color.black)
+            text = self.__font.render(airport.name, True, self.__text_color)
             display.blit(text, (airport_x + (self.__airport_size * 1.5), airport_y - self.__airport_size ))
